32f97f0cc2f47db9508bd97807c488781acc5eb8
Stanley Wintergreen <kepocnhh@protonmail.com>
2026-03-20 13:19:30 +0300

    java:app run

--- src/main/bash/java/app/init.sh
+++ src/main/bash/java/app/init.sh
@@ -85,4 +85,80 @@ fi
 
 #
 
-echo 'Not implemented!'; exit 1 # todo
+mkdir app
+
+#
+
+ISSUER='app/.gitignore'
+
+if test -f "${ISSUER}"; then
+ echo "File \"${ISSUER}\" exists!"; exit 1; fi
+
+echo -n "\
+/*
+!/src
+!.gitignore
+!build.gradle.kts
+" > "${ISSUER}"
+
+if [[ ! -f "${ISSUER}" ]]; then
+ echo "No file \"${ISSUER}\"!"; exit 1
+elif [[ ! -s "${ISSUER}" ]]; then
+ echo "File \"${ISSUER}\" is empty!"; exit 1
+fi
+
+#
+
+echo 'Enter project namespace:'
+read -r PROJECT_NAMESPACE
+
+#
+
+ISSUER='app/build.gradle.kts'
+
+if test -f "${ISSUER}"; then
+ echo "File \"${ISSUER}\" exists!"; exit 1; fi
+
+echo -n "\
+repositories.mavenCentral()
+
+plugins {
+    kotlin(\"jvm\")
+}
+
+tasks.register<JavaExec>(\"run\") {
+    classpath = sourceSets[\"main\"].runtimeClasspath
+    mainClass = \"${PROJECT_NAMESPACE}.AppKt\"
+}
+" > "${ISSUER}"
+
+if [[ ! -f "${ISSUER}" ]]; then
+ echo "No file \"${ISSUER}\"!"; exit 1
+elif [[ ! -s "${ISSUER}" ]]; then
+ echo "File \"${ISSUER}\" is empty!"; exit 1
+fi
+
+#
+
+mkdir -p "app/src/main/kotlin/${PROJECT_NAMESPACE//.///}"
+
+#
+
+ISSUER="app/src/main/kotlin/${PROJECT_NAMESPACE//.///}/App.kt"
+
+if test -f "${ISSUER}"; then
+ echo "File \"${ISSUER}\" exists!"; exit 1; fi
+
+echo -n "\
+package ${PROJECT_NAMESPACE}
+
+fun main() {
+    println(\"Hello ${PROJECT_NAME}!\")
+}
+" > "${ISSUER}"
+
+if [[ ! -f "${ISSUER}" ]]; then
+ echo "No file \"${ISSUER}\"!"; exit 1
+elif [[ ! -s "${ISSUER}" ]]; then
+ echo "File \"${ISSUER}\" is empty!"; exit 1
+fi
